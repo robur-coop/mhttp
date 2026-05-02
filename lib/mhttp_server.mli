e7461e7845dfcd39aea3edd1599c8710c5e2ebd6
Calascibetta Romain <romain.calascibetta@gmail.com>
2026-05-02 14:49:54 +0200

    Use the lastest version of httpcats

--- lib/mhttp_server.mli
+++ lib/mhttp_server.mli
@@ -37,12 +37,13 @@ type request = {
     ["https"]) and a headers. *)
 
 type body = [ `V1 of H1.Body.Writer.t | `V2 of H2.Body.Writer.t ]
+type conn = [ `H1 of H1.Server_connection.t | `H2 of H2.Server_connection.t ]
 type reqd = [ `V1 of H1.Reqd.t | `V2 of H2.Reqd.t ]
 
 type error_handler =
   [ `V1 | `V2 ] -> ?request:request -> error -> (Headers.t -> body) -> unit
 
-type handler = flow -> reqd -> unit
+type handler = flow -> conn -> reqd -> unit
 
 val clear :
      ?stop:stop
